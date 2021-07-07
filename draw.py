55686928a43a83e3a0f147237f1992df5b0ed717
Kristal Kung <kristalkung@gmail.com>
2021-07-07 01:47:04 +0000

    edited add_shape() to add shapes into a dictionary of shapes within canvas object

--- draw.py
+++ draw.py
@@ -1,46 +1,3 @@
-class Canvas:
-
-    def __init__(self, height, width):
-        self.height = height
-        self.width = width
-    
-    def print_blank_canvas(self):
-        """Prints the canvas"""
-        for i in range(0, self.height):
-            canvas_width = '.' * self.width
-            print(canvas_width)
-    
-    def add_shape(self, shape):
-        start_x = shape.start_x
-        start_y = shape.start_y
-        end_x = shape.end_x
-        end_y = shape.end_y
-        fill_char = shape.fill_char
-
-        insert_shape_width = fill_char * (end_x - start_x)
-        insert_shape_height = end_y - start_y
-
-        for i in range(0, end_y - start_y):
-            canvas_width = '.' * self.width
-            canvas_width_shape = canvas_width[:start_x] + insert_shape_width + canvas_width[end_x:]
-            print(canvas_width_shape)
-
-        for i in range(0, self.height - end_y):
-            canvas_width = '.' * self.width
-            
-            print(canvas_width)
-
-
-
-    def clear_all_shapes(self):
-        
-        if shapes_in_canvas:
-            for shape in shapes_in_canvas:
-                del shape
-        shapes_in_canvas = []
-        print_canvas()
-        #todo
-
 class Shape:
     pass
 
@@ -64,3 +21,58 @@ class Rectangle(Shape):
         else:
             start_y += self.num
             end_y += self.num
+
+class Canvas:
+    shape_dict = {}
+
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+    
+    def print_blank_canvas(self):
+        """Prints the canvas"""
+        for i in range(0, self.height):
+            canvas_width = '.' * self.width
+            print(canvas_width)
+    
+    def add_shape(self, shape):
+        start_x = shape.start_x
+        start_y = shape.start_y
+        end_x = shape.end_x
+        end_y = shape.end_y
+        fill_char = shape.fill_char
+
+        shape_info = {'start_x': shape.start_x, 
+                 'start_y': shape.start_y, 
+                 'end_x': shape.end_x,
+                 'end_y': shape.end_y,
+                 'fill_char': shape.fill_char}
+        
+        self.shape_dict[shape] = shape_info
+        
+        # insert_shape_width = fill_char * (end_x - start_x)
+        # insert_shape_height = end_y - start_y
+
+        # for i in range(0, end_y - start_y):
+        #     canvas_width = '.' * self.width
+        #     canvas_width_shape = canvas_width[:start_x] + insert_shape_width + canvas_width[end_x:]
+        #     print(canvas_width_shape)
+
+        # for i in range(0, self.height - end_y):
+        #     canvas_width = '.' * self.width
+            
+        #     print(canvas_width)
+
+
+
+
+
+    def clear_all_shapes(self):
+        
+        if shapes_in_canvas:
+            for shape in shapes_in_canvas:
+                del shape
+        shapes_in_canvas = []
+        print_canvas()
+        #todo
+
